9dbc80af7c8594a8feb869554b5a4d9c1e7fef4a
chungkim271 <chungkim271@gmail.com>
2020-01-16 16:06:43 -0500

    changed hover to slidedown

---
+++ app/static/js/custom.js
@@ -0,0 +1,48 @@
+$(function() {
+    $('#fileupload').fileupload({
+        width: 300,
+        url: 'upload',
+        dataType: 'json',
+        replaceFileInput:false,
+        add: function(e, data) {
+            data.submit()
+        },
+        success: function(response, status) {
+            console.log(response.filename)
+            var filePath = 'static/uploads/' + response.filename
+            $('#imgUpload').attr('src',filePath)
+            $('#container-pred').removeClass("hidden")
+            $('#filePath').text(filePath)
+            $('#pred').text(' ')
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    })
+})
+$(function() {
+    $('#predict').click(function(e) {
+        e.preventDefault()
+        $('#predict').text('Predicting...')
+        data = {f_name:$('#filePath')[0].textContent}
+        $.ajax({
+            type: "POST",
+            url: "/predict",
+            data: JSON.stringify(data, null, '\t'),
+            contentType: 'application/json;charset=UTF-8',
+            success: function(response, status) {
+                pred = response.pred
+                $('#pred').text(pred)
+                $('#predict').text('Predict')
+            },
+            error: function(result) {
+                console.log(error)
+            }
+        })
+    })
+})
+$(function() {
+    $('#slide-down').click(function() {
+        $('#txt-content').slideToggle()
+    })
+})
